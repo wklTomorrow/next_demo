55f690f7eaab13f3a5a7cc15bcaa3d1721745ceb
wangkunlun <13013988673@163.com>
2022-03-04 17:46:46 +0800

    feat: 从请求种获取头部信息

--- common/app.js
+++ common/app.js
@@ -2,6 +2,7 @@ import Content from './context'
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import { enableBatching } from 'redux-batched-actions';
+// const isMobile = () => (window?.navigator?.userAgent?.match(/(iPhone|iPod|Android|ios|iOS|iPad|Backerry|WebOS|Symbian|Windows Phone|Phone)/i))
 
 export default function WrapperComponent(
     {
@@ -10,16 +11,44 @@ export default function WrapperComponent(
         ...params
     }
 ) {
-    return function Wrap(props) {
+    return function Wrap(props = {}) {
         let stores = createStore(combineReducers({store}));
+        const {isMobile} = props
+        const item = (
+        <Provider store={stores}>
+            <Content.Provider value={{
+                str: 'hello world',
+                isMobile: isMobile
+            }}>
+                <Component {...props} {...params}/>
+            </Content.Provider>
+        </Provider>)
+        if (isMobile) {
+            return item
+        }
         return (
-            <Provider store={stores}>
-                <Content.Provider value={{
-                    str: 'hello world'
+            <div style={{
+                display: 'flex',
+                flexDirection: 'row',
+                height: '100vh',
+                width: '100vw'
+            }}>
+                <div style={{
+                    flex: 1
+                }}>left</div>
+                <div style={{
+                    flex: 1,
+                    border: '1px solid red',
+                    boxSizing: 'border-box'
                 }}>
-                    <Component {...props} {...params}/>
-                </Content.Provider>
-            </Provider>
+                    {item}
+                </div>
+                <div style={{
+                    flex: 1
+                }}>
+                    right
+                </div>
+            </div>
         )
     }
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
+import App from 'next/app'
 import '../styles/globals.css'
+function MyApp({ Component, pageProps, isMobile }) {
+  return <Component {...pageProps} isMobile={isMobile} />
+}
 
+MyApp.getInitialProps = async (appContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
+  const isMobile = () => (/(iPhone|iPod|Android|ios|iOS|iPad|Backerry|WebOS|Symbian|Windows Phone|Phone)/i.test(appContext.ctx.req.headers['user-agent']))
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return { ...appProps, isMobile: isMobile()}
 }
 
 export default MyApp
